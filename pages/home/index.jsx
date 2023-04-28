f24c604e361562307397da4b67cd729aba1014b5
Pavan <pavansargar36@gmail.com>
2023-04-28 23:24:56 +0530

    test

--- pages/home/index.jsx
+++ pages/home/index.jsx
@@ -118,7 +118,7 @@ const Home = ({ posts }) => {
                 <PostCard
                   key={item?.projectID}
                   title={item?.postTitle}
-                  address={item?.projectURLs[0]}
+                  address={item?.projectURLs[0] ?? "dummy.com"}
                   description={item?.postDescription}
                   name={`${item?.users?.name}`}
                   // userName={item?.user?.userName}
