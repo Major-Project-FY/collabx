5b0ae20f65cec80d6779632cb81d604451fdc279
Pavan <pavansargar36@gmail.com>
2023-04-28 23:52:50 +0530

    fix

---
+++ utils/getRandomColor.js
@@ -0,0 +1,28 @@
+export function getRandomColor() {
+    var letters = "0123456789ABCDEF";
+    var color = "#";
+    for (var i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    // Check if the color is dark or light
+    var r = parseInt(color.slice(1, 3), 16);
+    var g = parseInt(color.slice(3, 5), 16);
+    var b = parseInt(color.slice(5, 7), 16);
+    var brightness = (r * 299 + g * 587 + b * 114) / 1000;
+    if (brightness < 128) {
+      // If the color is too dark, make it lighter
+      color = lighten(color, 30);
+    }
+    return color;
+  }
+  
+  function lighten(color, percent) {
+    var num = parseInt(color.slice(1), 16);
+    var amt = Math.round(2.55 * percent);
+    var R = (num >> 16) + amt;
+    var G = (num >> 8 & 0x00FF) + amt;
+    var B = (num & 0x0000FF) + amt;
+    var newColor = "#" + (0x1000000 + (R < 255 ? R < 1 ? 0 : R : 255) * 0x10000 + (G < 255 ? G < 1 ? 0 : G : 255) * 0x100 + (B < 255 ? B < 1 ? 0 : B : 255)).toString(16).slice(1);
+    return newColor;
+  }
+  
