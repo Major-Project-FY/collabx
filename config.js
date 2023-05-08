302abeb5abcfac033f7533ed3282d1c93d1ce96a
Mayuresh-loves-cpp <mvshinde640@gmail.com>
2023-05-08 12:23:34 +0530

    backend url change

--- config.js
+++ config.js
@@ -11,7 +11,7 @@ const Config = {
   root:
     env === "development"
       ? "http://127.0.0.1:5000"
-      : "https://colabx.tech",
+      : "https://api.colabx.tech",
   auth: {
     login: "/auth/user/login",
     signup: "/auth/user/signup",
